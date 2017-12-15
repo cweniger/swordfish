a2a5459f96c6c9ca28ce4be87a4d32db56ce19df
Christoph Weniger <c.weniger@uva.nl>
2017-12-15 17:17:08 +0100

    Update setup.py to v1.0.1

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='swordfish',
-    version='1.0.0',
+    version='1.0.1',
     description='Predicting the Information Yield of Counting Experiments.',
     author='Thomas Edwards and Christoph Weniger',
     author_email='c.weniger@uva.nl',
     url = 'https://github.com/cweniger/swordfish',
-    download_url = 'https://github.com/cweniger/swordfish/archive/v1.0.0.tar.gz',
+    download_url = 'https://github.com/cweniger/swordfish/archive/v1.0.1.tar.gz',
     packages=['swordfish'],
     package_data={'swordfish': [] },
     long_description=open('README.md').read(),
